3b83d651e60fd2e3abc4e46706b07c651cf6049e
Manul <manuljain16@gmail.com>
2020-10-20 20:53:26 +0530

    Added multiple times flipping for a coin

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -3,12 +3,23 @@
 # Welcome message
 echo "Welcome to Flip Coin Simulation Program."
 
-# Flipping a coin
-coin=$((RANDOM%2))
+#will count the number of heads and tails
+counth=0
+countt=0
 
-if [ $coin -eq 1 ]
-then
-	echo "it's a head..head wins"
-else
-	echo "it's a tail..tail wins"
-fi
+for (( i=0; i<21; i++ ))
+do
+    # Flipping a coin
+    coin=$((RANDOM%2))
+
+    if [ $coin -eq 1 ]
+    then
+        counth=$(($counth+1))
+    else
+        countt=$(($countt+1))
+    fi
+done
+
+# display win count
+echo "Head won $counth times"
+echo "Tail won $countt times"
