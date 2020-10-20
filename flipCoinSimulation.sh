be1984ad619e8b04ef3aaafaf2396799df0dbdbf
Manul <manuljain16@gmail.com>
2020-10-20 22:08:56 +0530

    Added a tie condition and loop till get difference of 2

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -18,20 +18,41 @@ do
     else
         countt=$(($countt+1))
     fi
-    if [[ $counth -eq 21 ]]
+    # condition for checking head count and tail count
+    if [ $counth -eq $countt ] || [ $countt -eq $counth ] 
     then
-	    echo "Head counts : $counth   Tail count : $countt"
-        count=$(($counth-$countt))
-        echo "Head won by $count"
-        break
-    elif [[ $countt -eq 21 ]]
-    then
-	    echo "Head counts : $counth   Tail count : $countt"
-        count=$(($countt-$counth))
-        echo "Tail won by $count"
+        echo "Head count : $counth   Tail count : $countt"
+        echo "head count and tail counts are equal .. it means it's a tie"
+        echo "============================================================="
         break
     else 
         continue
     fi
 done
-
+# loop while difference of head count and tail count is 2
+while true
+do
+    coin=$((RANDOM%2))
+    
+    if [ $coin -eq 1 ]
+    then
+        counth=$(($counth+1))
+    else
+        countt=$(($countt+1))
+    fi
+    if [ $(($counth-$countt)) -eq 2 ]
+    then
+        echo "Head count : $counth   Tail count : $countt"
+        echo "difference between head count and tail count is 2"
+        echo "head won"
+        break
+    elif [ $(($countt-$counth)) -eq 2 ]
+    then
+        echo "Head count : $counth   Tail count : $countt"
+        echo "difference between tail count and head count is 2"
+        echo "tail won"
+        break
+    else
+        continue
+    fi
+done
