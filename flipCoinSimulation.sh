41b33f686ac00f9950aff656e1821c89606511ce
Manul <manuljain16@gmail.com>
2020-10-20 20:46:31 +0530

    Added simulator for flipping a coin to display head or tail won

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -2,3 +2,13 @@
 
 # Welcome message
 echo "Welcome to Flip Coin Simulation Program."
+
+# Flipping a coin
+coin=$((RANDOM%2))
+
+if [ $coin -eq 1 ]
+then
+	echo "it's a head..head wins"
+else
+	echo "it's a tail..tail wins"
+fi
